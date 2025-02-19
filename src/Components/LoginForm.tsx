1324ae573e8c2af7b51119d276927c20819eedca
Kittipun Krajng <kittipun.k@outlook.com>
2025-02-19 08:33:29 -0500

    Finish loginForm component

--- src/Components/LoginForm.tsx
+++ src/Components/LoginForm.tsx
@@ -1,4 +1,9 @@
-import React from "react";
+import React, {FormEvent, useState} from "react";
+
+type loginModel = {
+    username: string,
+    password: string
+}
 
 interface LoginFormProps{
 
@@ -6,8 +11,36 @@ interface LoginFormProps{
 
 const LoginForm: React.FC<LoginFormProps> = ({}) =>{
 
-    return <form>
-        
+    const [input, setInput] = useState<loginModel>({
+        username:'',
+        password:''
+    });
+
+    const onSubmit = (e:FormEvent) => {
+        e.preventDefault();
+        console.log(input);
+    }
+
+    return <form onSubmit={(e)=>{onSubmit(e)}}>
+
+        <input 
+            name="username"
+            type="text" 
+            placeholder="USERNAME"
+            required
+            value={input.username} 
+            onChange={e=>{setInput({...input, [e.target.name]: e.target.value})}}
+        />
+
+        <input 
+            name="password"
+            type="password" 
+            placeholder="PASSWORD"
+            required
+            value={input.password} 
+            onChange={e=>{setInput({...input, [e.target.name]: e.target.value})}}
+        />
+        <button type="submit">Submit</button>
     </form>
 }
 
