c116bfd7c36e0342a166228036395d7fe2f3338c
Kittipun Krajng <kittipun.k@outlook.com>
2025-02-20 11:54:55 -0500

    Add token and signout button in taskPage

--- src/Pages/Task.tsx
+++ src/Pages/Task.tsx
@@ -1,14 +1,25 @@
-import React from "react";
-import { RouteProps } from "react-router-dom";
+import React,{ useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 interface TaskPageProps {
-    RouteProps: RouteProps
+
 }
 
-const TaskPage: React.FC<TaskPageProps> = ({ RouteProps }) => {
+const TaskPage: React.FC<TaskPageProps> = ({ }) => {
+
+    const navigate = useNavigate();
+
+    const [token, setToken] = useState(localStorage.getItem('token'));
+
+    const onSignOut = () => {
+        localStorage.removeItem('token');
+        navigate('/');
+    }
 
     return <div>
-
+        <button onClick={onSignOut}>Sign out</button>
+        
     </div>
 }
 
