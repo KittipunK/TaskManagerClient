4b599df39f9a5f445d2225f579f2150fd6a2c576
Kittipun Krajng <kittipun.k@outlook.com>
2025-02-22 22:53:44 -0500

    Add fetch to checking complete box

--- src/Components/TaskTemplate.tsx
+++ src/Components/TaskTemplate.tsx
@@ -12,27 +12,65 @@ interface TaskTemplateProps{
 const TaskTemplate: React.FC<TaskTemplateProps> = ({id, title, description, isComplete, userId, token}) =>{
 
     const [isEdit, setIsEdit] = useState<boolean>(false);
+    const [data, setData] = useState<TaskTemplateProps>({
+        id: id,
+        title: title,
+        description: description,
+        isComplete: isComplete,
+        userId: userId,
+        token: token
+    });
 
     const onCheckboxChange = () =>{
-        
+        fetch(`http://localhost:5000/tasks/${data.id}`,{
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+                token: data.token,
+                column: 'isComplete',
+                value: !data.isComplete
+            })
+        }).then(async (res)=>{
+            if(res.status==200){
+                console.log('complete');
+            }
+        }).catch(err=>{console.log(err);
+        })
+        window.location.reload();
     }
 
-    const onEdit = () =>{
+    const onIsEdit = () =>{
+        setIsEdit(!isEdit);
+    }
 
+    const onEditChange = () =>{
+
+    }
+
+    const onSave = () =>{
+
+        window.location.reload();
     }
 
     const onDelete = () =>{
 
+        window.location.reload();
     }
 
-    return  <li key={id} className="taskTemplate">
-        <input type="checkbox" onChange={onCheckboxChange} checked={(isComplete?true:false)}/>
+    return  <li key={data.id} className="taskTemplate">
+        <input type="checkbox" disabled={isEdit} onChange={onCheckboxChange} checked={(data.isComplete?true:false)}/>
         <div className="detail">
-            <h4>{title}</h4>
-            <p>{description.length>0?description:"no description"}</p>
+            <h4>{data.title}</h4>
+            <p>{data.description.length>0?data.description:"no description"}</p>
         </div>
-        <button onClick={()=>onEdit()}>edit</button>
-        <button onClick={()=>onDelete()}>delete</button>
+        {(isEdit)
+        ?<button onClick={()=>onSave()}>save</button>
+        :<button onClick={()=>onIsEdit()}>edit</button>
+        }
+        <button onClick={()=>onDelete()} disabled={isEdit}>delete</button>
     </li>
 }
 
